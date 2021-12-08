e15af93b6e44ab9b3ab4d1ea857be511797262b1
mark yasser <markyasser2011@gmail.com>
2021-12-08 22:26:15 +0200

    added a small log in page 2

--- Hospital/LoginPage.Designer.cs
+++ Hospital/LoginPage.Designer.cs
@@ -52,6 +52,7 @@ namespace Hospital
             this.password.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.password.Location = new System.Drawing.Point(501, 378);
             this.password.Name = "password";
+            this.password.PasswordChar = '*';
             this.password.Size = new System.Drawing.Size(222, 28);
             this.password.TabIndex = 1;
             // 
