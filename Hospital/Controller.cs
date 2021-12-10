fdcccd40f828200be3cdab589ab1faf50b9e8a5d
Bemoi Erian <bemoi.erian@gmail.com>
2021-12-11 01:42:14 +0200

    Added initial froms, added controller and DBmanager files

---
+++ Hospital/Controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Hospital
+{
+    class Controller
+    {
+        DBManager dbMan;
+        public Controller()
+        {
+            dbMan = new DBManager();
+        }
+       
+        public void TerminateConnection()
+        {
+            dbMan.CloseConnection();
+        }
+    }
+}
+
--- Hospital/Program.cs
+++ Hospital/Program.cs
@@ -17,6 +17,11 @@ namespace Hospital
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new LoginPage());
+            //Application.Run(new Admin());
+            //Application.Run(new Receptionist());
+            //Application.Run(new Doctor());
+            //Application.Run(new Pharmacist());
+            //Application.Run(new Nurse());
         }
     }
 }
