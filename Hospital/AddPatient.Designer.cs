cc213cf2a53a37d09d6ff5a79885ba2217fc9336
peter_atef@stud.cu.edu.eg <peter_atef@stud.cu.edu.eg>
2021-12-13 21:02:53 +0200

    peter

---
+++ Hospital/AddPatient.Designer.cs
@@ -0,0 +1,246 @@
+﻿
+namespace Hospital
+{
+    partial class AddPatient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Fname_textBox = new System.Windows.Forms.TextBox();
+            this.Lname_textBox = new System.Windows.Forms.TextBox();
+            this.phno_textBox = new System.Windows.Forms.TextBox();
+            this.minit_textBox = new System.Windows.Forms.TextBox();
+            this.Address_textBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ID_textBox = new System.Windows.Forms.TextBox();
+            this.gender_comboBox = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(12, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // Fname_textBox
+            // 
+            this.Fname_textBox.Location = new System.Drawing.Point(194, 50);
+            this.Fname_textBox.Name = "Fname_textBox";
+            this.Fname_textBox.Size = new System.Drawing.Size(253, 22);
+            this.Fname_textBox.TabIndex = 8;
+            // 
+            // Lname_textBox
+            // 
+            this.Lname_textBox.Location = new System.Drawing.Point(194, 279);
+            this.Lname_textBox.Name = "Lname_textBox";
+            this.Lname_textBox.Size = new System.Drawing.Size(253, 22);
+            this.Lname_textBox.TabIndex = 9;
+            // 
+            // phno_textBox
+            // 
+            this.phno_textBox.Location = new System.Drawing.Point(698, 146);
+            this.phno_textBox.Name = "phno_textBox";
+            this.phno_textBox.Size = new System.Drawing.Size(200, 22);
+            this.phno_textBox.TabIndex = 11;
+            // 
+            // minit_textBox
+            // 
+            this.minit_textBox.Location = new System.Drawing.Point(194, 151);
+            this.minit_textBox.Name = "minit_textBox";
+            this.minit_textBox.Size = new System.Drawing.Size(253, 22);
+            this.minit_textBox.TabIndex = 14;
+            // 
+            // Address_textBox
+            // 
+            this.Address_textBox.Location = new System.Drawing.Point(194, 400);
+            this.Address_textBox.Name = "Address_textBox";
+            this.Address_textBox.Size = new System.Drawing.Size(253, 22);
+            this.Address_textBox.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(12, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 25);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Minit";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(12, 274);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(130, 25);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Last Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(525, 53);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(126, 25);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Birth Date";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(698, 53);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 19;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(526, 146);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(124, 25);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Phone no.";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(12, 382);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(103, 25);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(525, 276);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 25);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Patient ID";
+            // 
+            // ID_textBox
+            // 
+            this.ID_textBox.Location = new System.Drawing.Point(698, 281);
+            this.ID_textBox.Name = "ID_textBox";
+            this.ID_textBox.Size = new System.Drawing.Size(200, 22);
+            this.ID_textBox.TabIndex = 23;
+            // 
+            // gender_comboBox
+            // 
+            this.gender_comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.gender_comboBox.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.gender_comboBox.Location = new System.Drawing.Point(698, 395);
+            this.gender_comboBox.Name = "gender_comboBox";
+            this.gender_comboBox.Size = new System.Drawing.Size(200, 24);
+            this.gender_comboBox.TabIndex = 24;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(542, 395);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 25);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Gender";
+            // 
+            // AddPatient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 512);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.gender_comboBox);
+            this.Controls.Add(this.ID_textBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Address_textBox);
+            this.Controls.Add(this.minit_textBox);
+            this.Controls.Add(this.phno_textBox);
+            this.Controls.Add(this.Lname_textBox);
+            this.Controls.Add(this.Fname_textBox);
+            this.Controls.Add(this.label1);
+            this.Name = "AddPatient";
+            this.Text = "AddPatient";
+            this.Load += new System.EventHandler(this.AddPatient_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Fname_textBox;
+        private System.Windows.Forms.TextBox Lname_textBox;
+        private System.Windows.Forms.TextBox phno_textBox;
+        private System.Windows.Forms.TextBox minit_textBox;
+        private System.Windows.Forms.TextBox Address_textBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ID_textBox;
+        private System.Windows.Forms.ComboBox gender_comboBox;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ Hospital/AddPatient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hospital
+{
+    public partial class AddPatient : Form
+    {
+        public AddPatient()
+        {
+            InitializeComponent();
+        }
+
+        private void AddPatient_Load(object sender, EventArgs e)
+        {
+            dateTimePicker1.CustomFormat= "yyyy-MM-dd";
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+        }
+    }
+}
