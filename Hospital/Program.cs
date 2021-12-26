7be5543da6bca44082e3daf5e77e9c39f05b037f
mark yasser <markyasser2011@gmail.com>
2021-12-26 03:16:03 +0200

    Admin and Doctor

--- Hospital/Program.cs
+++ Hospital/Program.cs
@@ -16,16 +16,14 @@ namespace Hospital
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-<<<<<<< HEAD
             Application.Run(new LoginPage());
-=======
+
             //Application.Run(new LoginPage());
->>>>>>> 065c3ef6e06e6434b5e1f1a22b5822abe66aab31
             //Application.Run(new Admin());
             //Application.Run(new LoginPage());
             //Application.Run(new Admin());
             //Application.Run(new Receptionist());
-            Application.Run(new Doctor());
+            //Application.Run(new Doctor());
             //Application.Run(new Pharmacist());
             //Application.Run(new Nurse());
         }
